fce2e4b0a7eb5edc3d07d5f7af22e67707ec86a1
Abhi Gupta <abhi.gupta@columbia.edu>
2021-08-09 20:20:37 -0500

    Tanmay initial commit

---
+++ tanmay/Track1/tf2_keras_vggface/version.py
@@ -0,0 +1,8 @@
+__version__ = '0.6'
+
+def pretty_versions():
+    import tensorflow.keras
+    import tensorflow as tf
+    k_version = tensorflow.keras.__version__
+    t_version = tf.__version__
+    return "keras-vggface : {}, keras : {} , tensorflow : {} ".format(__version__, k_version, t_version)
